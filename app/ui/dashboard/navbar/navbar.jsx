80f48fbc144823cb403e60785014853d3d925934
Wajud <wajudkareem757@gmail.com>
2024-05-29 22:51:35 +0100

    just installed recharts for charts

--- app/ui/dashboard/navbar/navbar.jsx
+++ app/ui/dashboard/navbar/navbar.jsx
@@ -1,7 +1,30 @@
+"use client";
 import React from "react";
+import styles from "./navbar.module.css";
+import { usePathname } from "next/navigation";
+import {
+  MdNotifications,
+  MdOutlineChat,
+  MdPublic,
+  MdSearch,
+} from "react-icons/md";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const pathname = usePathname();
+  return (
+    <div className={styles.container}>
+      <div>{pathname.split("/").pop()}</div>
+      <div className={styles.search}>
+        <MdSearch />
+        <input type="text" placeholder="Search..." className={styles.input} />
+        <div className={styles.icons}>
+          <MdOutlineChat size={20} />
+          <MdNotifications size={20} />
+          <MdPublic size={20} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
